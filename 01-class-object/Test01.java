5231ea7feac44c2d953a8918853ce9b3bdc5f6c8
Ankush <ankushgt25@gmail.com>
2024-02-22 16:54:44 +0530

    java

--- 01-class-object/Test01.java
+++ 01-class-object/Test01.java
@@ -5,14 +5,24 @@ class Student {
     int age;
 
     public void getInfo() {
-        System.out.println(name);
-        System.out.println(age);
+        String a = this.name;
+        int b = this.age;
+        System.out.println(a);
+        System.out.println(b);
     }
 
 }
 
 public class Test01 {
     public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Ankush";
+        s1.age = 21;
 
+        Student s2 = new Student();
+        s2.name = "Tiwari";
+        s2.age = 20;
+        s1.getInfo();
+        s2.getInfo();
     }
 }
