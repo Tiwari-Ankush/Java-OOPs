7a41ad91ca9dd29f9ae3f948889bc23620526d44
Ankush <ankushgt25@gmail.com>
2024-02-22 17:48:04 +0530

    java

---
+++ 03-polymorphism/Poly01.java
@@ -0,0 +1,14 @@
+// Compile time PolyMorphism
+// METHOD OVERRLOADING 
+
+class Student {
+    String name;
+    int age;
+
+}
+
+public class Poly01 {
+    public static void main(String[] args) {
+
+    }
+}
