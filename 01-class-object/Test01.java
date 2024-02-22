080a77bb3bc272c49c47cdc7fccf056a99cb429b
Ankush <ankushgt25@gmail.com>
2024-02-22 16:46:31 +0530

    java

---
+++ 01-class-object/Test01.java
@@ -0,0 +1,18 @@
+// classes and objects practice
+
+class Student {
+    String name;
+    int age;
+
+    public void getInfo() {
+        System.out.println(name);
+        System.out.println(age);
+    }
+
+}
+
+public class Test01 {
+    public static void main(String[] args) {
+
+    }
+}
