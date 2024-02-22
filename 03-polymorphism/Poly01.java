d0047f3dfd3af7a5443477d7b0237fb02b9f2187
Ankush <ankushgt25@gmail.com>
2024-02-22 18:45:17 +0530

    java

--- 03-polymorphism/Poly01.java
+++ 03-polymorphism/Poly01.java
@@ -5,10 +5,24 @@ class Student {
     String name;
     int age;
 
+    public void printInfo(String name) {
+        System.out.println(name);
+    }
+
+    public void printInfo(int age) {
+        System.out.println(age);
+    }
+
+    public void printInfo(String name, int age) {
+        System.out.println(name + " " + age);
+    }
 }
 
 public class Poly01 {
     public static void main(String[] args) {
-
+        Student s1 = new Student();
+        s1.printInfo("Ankush");
+        s1.printInfo(21);
+        s1.printInfo("TIwari", 21);
     }
 }
